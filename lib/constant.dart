eb5bf3811a1f85a1de016547ecc7e3792b6cc98d
Amelguedjali <102831988+Amelguedjali@users.noreply.github.com>
2022-12-24 19:59:00 +0100

    Add files via upload

---
+++ lib/constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color blueC = Color(0xFF2F89FC);
+Color blueF = Color(0XFF294F81);
+Color white = Color(0xFFF5F5F5);
+Color black = Color(0xFF000000);
+Color griy = Color(0xFF616161);
